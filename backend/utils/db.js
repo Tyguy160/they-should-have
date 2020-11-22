4c04770878c193886571c18048bb490c4e48679e
Tyler Maschino <tylermaschino@gmail.com>
2020-11-22 08:10:33 -0500

    Separated frontend and backend

--- backend/utils/db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const url = 'mongodb+srv://user:pass1234@cluster0.prae2.mongodb.net/test';
-
-const connect = () => {
-  return mongoose.connect(url, { useNewUrlParser: true });
-};
-
-module.exports = { connect };
