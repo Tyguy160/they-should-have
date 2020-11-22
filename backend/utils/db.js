a94e79a1c5c31c64a07806071d6b9d84350699a9
Tyler Maschino <tylermaschino@gmail.com>
2020-11-22 07:41:21 -0500

    Updated db file

--- backend/utils/db.js
+++ backend/utils/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const url = 'mongodb://localhost/test';
+const url = 'mongodb+srv://user:pass1234@cluster0.prae2.mongodb.net/test';
 
 const connect = () => {
   return mongoose.connect(url, { useNewUrlParser: true });
